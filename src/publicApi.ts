9fd07b78e77a0c680c3a1468b9fde0c9fd8e7090
Wenzhao Hu <wendellhu95@gmail.com>
2023-01-29 19:08:34 +0800

    :bug: fix typescript could not refer to the correct type

--- src/publicApi.ts
+++ src/publicApi.ts
@@ -5,25 +5,28 @@ export { forwardRef } from './dependencyForwardRef'
 export { Injector } from './injector'
 export { SkipSelf, Self } from './dependencyLookUp'
 export { DependencyPair, Dependency } from './dependencyCollection'
-export { DependencyIdentifier } from './dependencyIdentifier'
+export {
+  DependencyIdentifier,
+  IdentifierDecorator,
+} from './dependencyIdentifier'
 export { Disposable } from './dispose'
 export { setDependencies } from './dependencyDeclare'
 export { registerSingleton } from './dependencySingletons'
 export { WithNew } from './dependencyWithNew'
 export {
-    AsyncDependencyItem,
-    AsyncHook,
-    ClassDependencyItem,
-    Ctor,
-    DependencyItem,
-    FactoryDependencyItem,
-    isAsyncDependencyItem,
-    isAsyncHook,
-    isClassDependencyItem,
-    isCtor,
-    isFactoryDependencyItem,
-    isValueDependencyItem,
-    SyncDependencyItem,
-    ValueDependencyItem,
+  AsyncDependencyItem,
+  AsyncHook,
+  ClassDependencyItem,
+  Ctor,
+  DependencyItem,
+  FactoryDependencyItem,
+  isAsyncDependencyItem,
+  isAsyncHook,
+  isClassDependencyItem,
+  isCtor,
+  isFactoryDependencyItem,
+  isValueDependencyItem,
+  SyncDependencyItem,
+  ValueDependencyItem,
 } from './dependencyItem'
 export { RediError } from './error'
