f29050211ec020a9581a9a7a0ae5fabb559bf0d3
Wenzhao Hu <wzhudev@gmail.com>
2024-10-29 10:52:32 +0800

    docs: move docs to main repo (#31)

---
+++ doc/pages/en-US/_meta.ts
@@ -0,0 +1,14 @@
+export default {
+  "index": {
+    title: "Introduction",
+    type: "page"
+  },
+  "docs": {
+    title: "Guides",
+    type: "page"
+  },
+  "blogs": {
+    title: "Blogs",
+    type: "page"
+  }
+}
---
+++ doc/theme.config.tsx
@@ -0,0 +1,43 @@
+export default {
+  docsRepositoryBase: 'https://github.com/wzhudev/redi-site/tree/main',
+  project: {
+    link: 'https://github.com/wzhudev/redi',
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: '%s – Redi'
+    }
+  },
+  logo: (
+    <>
+      <span style={{ marginLeft: '.4em', fontWeight: 800 }}>Redi</span>
+    </>
+  ),
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Redi" />
+      <meta property="og:description" content="A light-weight dependency injection library" />
+    </>
+  ),
+  i18n: [
+    { locale: "en-US", name: "English" },
+    { locale: "zh-CN", name: "简体中文" },
+  ],
+  toc: {
+    title: '',
+    backToTop: true
+  },
+  footer: {
+    content: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://wzhu.dev" target="_blank">
+          wzhudev
+        </a>
+        .
+      </span>
+    )
+  },
+  faviconGlyph: "💉"
+}
