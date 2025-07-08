1a30fe96cacb931ff619a1b7371e7d3437c1a266
Wenzhao Hu <12122021+wzhudev@users.noreply.github.com>
2025-07-08 17:55:21 +0800

    build: use rolldown to build redi

---
+++ rolldown.config.ts
@@ -0,0 +1,46 @@
+import { defineConfig } from 'rolldown';
+import { dts } from 'rolldown-plugin-dts';
+
+export default defineConfig([
+  {
+    input: 'src/index.ts',
+    output: {
+      // file: 'dist/index.mjs',
+      format: 'esm',
+      sourcemap: true,
+      dir: 'dist',
+    },
+    plugins: [dts()],
+  },
+  {
+    input: 'src/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+      // dir: 'dist',
+    },
+    // plugins: [dts()],
+  },
+  {
+    input: 'src/react-bindings/index.ts',
+    external: ['@wendellhu/redi'],
+    output: {
+      // file: 'dist/react-bindings/index.mjs',
+      format: 'esm',
+      dir: 'dist/react-bindings',
+      sourcemap: true,
+    },
+
+    plugins: [dts()],
+  },
+  {
+    input: 'src/react-bindings/index.ts',
+    external: ['@wendellhu/redi'],
+    output: {
+      file: 'dist/react-bindings/index.js',
+      format: 'cjs',
+      sourcemap: true,
+    },
+  },
+]);
