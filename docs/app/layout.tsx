a69c2753c0b4dad37291bb55c179a9d46318320c
Evan <12122021+wzhudev@users.noreply.github.com>
2025-12-21 22:23:30 +0800

    fix: fix style import

--- docs/app/layout.tsx
+++ docs/app/layout.tsx
@@ -3,7 +3,6 @@ import { Footer, Layout, Navbar } from 'nextra-theme-docs';
 import { Head } from 'nextra/components';
 import { getPageMap } from 'nextra/page-map';
 import 'nextra-theme-docs/style.css';
-import '../styles.css';
 
 export const metadata = {
   title: {
